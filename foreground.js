ed3095f0878a18f367f85b783a43a7080c030ffc
patryk.mauer <patryk.mauer@student.po.edu.pl>
2021-01-17 08:58:43 +0100

    The grid style works

--- foreground.js
+++ foreground.js
@@ -10,10 +10,16 @@ var spans = document.querySelectorAll(".new");
 
 
 var i;
-var bodyHtml=`<ul class="items list-unstyled">`;
+var bodyHtml=`
+<div class="slider-wrapper pull-right" data-toggle="tooltip" data-placement="left" data-html="true" data-title="&lt;strong&gt;Zoom Thumbnails&lt;/strong&gt;&lt;br&gt;&lt;small&gt;PicClick Exclusive&lt;/small&gt;" data-original-title="" title=""><small><span class="glyphicon glyphicon-th" aria-hidden="true"></span></small><div id="slider" class="noUi-extended noUi-target noUi-rtl noUi-horizontal noUi-connect"><div class="noUi-base"><div class="noUi-origin noUi-background" style="left: 0%;"><div class="noUi-handle noUi-handle-upper"></div></div></div></div><span class="glyphicon glyphicon-th-large" aria-hidden="true"></span></div>
+				</div>
+<div class= "container-fluid">
+    <ul class="items list-unstyled">`;
 for(i=0; i<spans.length;i++){
-   bodyHtml+= spans[i].parentElement.parentElement.parentElement.parentElement.innerHTML;
+    spans[i].parentElement.parentElement.parentElement.parentElement.setAttribute("itemprop","isSimilarTo");
+    spans[i].parentElement.parentElement.parentElement.parentElement.setAttribute("itemscope",'');
+    bodyHtml+= spans[i].parentElement.parentElement.parentElement.parentElement.outerHTML;
 }
-bodyHtml+=`</ul>`;
+bodyHtml+=`</ul></div>`;
 
 document.body.innerHTML =bodyHtml;
