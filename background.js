da74743bc5bfe5795e2cfa962a4df059a0d5fc28
patryk.mauer <patryk.mauer@student.po.edu.pl>
2021-01-20 08:36:37 +0100

    Urls provided, fr.be doesn't work

--- background.js
+++ background.js
@@ -1,14 +1,17 @@
+function isPicclick(url)
+{
+   var result = /^https:\/\/\picclick\.com/.test(url) || /^https:\/\/\picclick\.de/.test(url)|| /^https:\/\/\picclick\.at/.test(url) || /^https:\/\/\picclick\.com.au/.test(url)  || /^https:\/\/\picclick\.be/.test(url)|| /^https:\/\/\fr\.picclick\.be/.test(url)|| /^https:\/\/\picclick\.ca/.test(url)|| /^https:\/\/\picclick\.fr/.test(url)|| /^https:\/\/\picclick\.it/.test(url)|| /^https:\/\/\picclick\.ie/.test(url)|| /^https:\/\/\picclick\.nl/.test(url)|| /^https:\/\/\picclick\.es/.test(url)|| /^https:\/\/\picclick\.ch/.test(url)|| /^https:\/\/\picclick\.co.uk/.test(url);
+return result;
+}
+
 chrome.tabs.onActivated.addListener(tab => {
     chrome.tabs.get(tab.tabId, current_tab_info => {
         active_tab_id = tab.tabId;
         console.log(current_tab_info.url);
 
-        if (/^https:\/\/\picclick\.com/.test(current_tab_info.url)) {
+        if (isPicclick(current_tab_info.url)) {
             chrome.tabs.insertCSS(null, { file: './styles.css' });
-            chrome.tabs.executeScript(null, { file: './foreground.js' }, () => console.log('i injected'))
+            chrome.tabs.executeScript(null, { file: './foreground.js' }, (result) => console.log(result))
         }
     });
 });
-
-
-// chrome.tabs.executeScript(null, {file: './foreground.js'}, ()=>console.log('I injected'))
--- foreground.js
+++ foreground.js
@@ -15,8 +15,6 @@ function checkForMatch(array, propertyToMatch, valueToMatch, position){
 
 var i;
 var bodyHtml=`
-<div class="slider-wrapper pull-right" data-toggle="tooltip" data-placement="left" data-html="true" data-title="&lt;strong&gt;Zoom Thumbnails&lt;/strong&gt;&lt;br&gt;&lt;small&gt;PicClick Exclusive&lt;/small&gt;" data-original-title="" title=""><small><span class="glyphicon glyphicon-th" aria-hidden="true"></span></small><div id="slider" class="noUi-extended noUi-target noUi-rtl noUi-horizontal noUi-connect"><div class="noUi-base"><div class="noUi-origin noUi-background" style="left: 0%;"><div class="noUi-handle noUi-handle-upper"></div></div></div></div><span class="glyphicon glyphicon-th-large" aria-hidden="true"></span></div>
-				</div>
 <div class= "container-fluid">
     <ul class="items list-unstyled">`;
 for(i=0; i<list.length;i++){
